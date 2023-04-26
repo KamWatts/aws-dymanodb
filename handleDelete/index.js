b0e405d7d59f5e0204d5c4792ce9f9f5f0d3dd45
Kameron Watts <kam.watts@gmail.com>
2023-04-26 14:50:57 -0700

    delete function optimization

--- handleDelete/index.js
+++ handleDelete/index.js
@@ -1,28 +1,37 @@
 const dynamoose = require('dynamoose');
 
 const personSchema = new dynamoose.Schema({
+  id: {
+    type: String,
+    hashKey: true,
+  },
   name: String,
   age: String,
-  height: String
-})
+  height: String,
+});
 
 const PersonModel = dynamoose.model('People', personSchema);
+
 exports.handler = async (event) => {
   console.log('DELETE person EVENT OBJECT: ', event);
 
   let parameters = event.pathParameters;
   let responseBody = null;
 
-  if (parameters) {
-    console.log('REQUEST PATH PARAMS: ', parameters);
-    responseBody = await PersonModel.scan('id').eq(parameters['id']).exec();
+  if (event.httpMethod === 'DELETE' && parameters && parameters['id']) {
+    console.log('DELETE REQUEST PATH PARAMS: ', parameters);
+    await PersonModel.delete(parameters['id']);
+    responseBody = { message: 'Item deleted successfully' };
   } else {
     responseBody = await PersonModel.scan().exec();
   }
-  console.log('person FROM OUR TABLE: ', responseBody);
+
+  console.log('PERSONS FROM OUR TABLE: ', responseBody);
+
   const response = {
     statusCode: 200,
     body: JSON.stringify(responseBody),
   };
-  return response
-}
+  return response;
+};
+
